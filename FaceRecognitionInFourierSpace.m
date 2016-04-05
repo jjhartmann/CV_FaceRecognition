9377931fd5099c4f30cb2413878b394c81812899
jhartman <jhartman@sfu.ca>
2016-04-04 20:53:09 -0700

    Clean up code.

--- FaceRecognitionInFourierSpace.m
+++ FaceRecognitionInFourierSpace.m
@@ -1,27 +1,6 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Facial Recognition in Fourier Space
 
-image = imread('att_faces/s1/1.pgm');
-image2 = imread('att_faces/s1/2.pgm');
-image3 = imread('att_faces/s1/3.pgm');
-image4 = imread('att_faces/s1/4.pgm');
-image5 = imread('att_faces/s1/5.pgm');
-image6 = imread('att_faces/s1/6.pgm');
-image7 = imread('att_faces/s2/6.pgm');
-
-Y = fftshift(fft2(image)); 
-Y = (Y + fftshift(fft2(image2)))/2;
-Y = (Y + fftshift(fft2(image3)))/2;
-Y = (Y + fftshift(fft2(image4)))/2;
-Y = (Y + fftshift(fft2(image5)))/2;
-
-Y = Y/1000000;
-
-Y6 = fftshift(fft2(image6))/1000000;
-Y7 = fftshift(fft2(image7))/1000000;
-
-
-
 %% Create feature vectors for images.
 norm_const = 1000000;
 FeatureVectorMap = [];
