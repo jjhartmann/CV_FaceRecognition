ecc99fc49e521c20b7dc6471437f3512ed7f295b
jhartman <jhartman@sfu.ca>
2016-04-05 22:46:36 -0700

    Add: test cases.

--- FaceRecognitionInFourierSpace.m
+++ FaceRecognitionInFourierSpace.m
@@ -3,12 +3,13 @@
 
 %% Create feature vectors for images.
 norm_const = 1000000;
+K_VAL = -15;
 FeatureVectorMap = [];
 FeatureVectorMap(40).vec = [];
 for i = 1:40
     location = strcat('att_faces/s', int2str(i), '/');
     Y = zeros(128, 128);
-   for j = 1:5
+   for j = 1:8
        current = strcat(location, int2str(j), '.pgm');
        img = imread(current);
        
@@ -20,14 +21,14 @@ for i = 1:40
        Y = (Y + fftshift(fft2(imgpad)));
    end
    % avg
-   Y = Y/5;
+   Y = Y/8;
    
    [n, m] = size(Y);
    Y = Y((n/2):n, (m/2):m, 1);
    
    % remove higher frequencies. 
    T = rot90(Y);
-   T = tril(T, 0);
+   T = tril(T, K_VAL);
    T = rot90(T');
    
    YR = real(T);
@@ -44,7 +45,7 @@ while (sel > 40 || sel < 1)
 end
 
 % Gen random image form test set. 
-imgIndex = randi(5) + 5;
+imgIndex = randi(2) + 8;
 location = strcat('att_faces/s', int2str(sel), '/', int2str(imgIndex), '.pgm');
 
 testImage = imread(location);
@@ -58,7 +59,7 @@ testY = testY((n/2):n, (m/2):m, 1);
 
 % remove higher frequencies. 
 T = rot90(testY);
-T = tril(T, 0);
+T = tril(T, K_VAL);
 T = rot90(T');
 
 YR = real(T);
@@ -78,6 +79,42 @@ end
 disp(['Answer is image set: ', int2str(index)])
 
 
+%% TEST
 
+for i = 1:40
+    % Gen random image form test set. 
+    imgIndex = randi(2) + 8;
+    location = strcat('att_faces/s', int2str(i), '/', int2str(imgIndex), '.pgm');
+
+    testImage = imread(location);
+    [n, m] = size(testImage);
+    imgpad = padarray(testImage, [0, floor(128-m)/2], 'replicate', 'both');
+    imgpad = padarray(imgpad', [0 floor(128-n)/2], 'replicate', 'both')';
+
+    [n, m] = size(imgpad);
+    testY = fftshift(fft2(imgpad));
+    testY = testY((n/2):n, (m/2):m, 1);
+
+    % remove higher frequencies. 
+    T = rot90(testY);
+    T = tril(T, K_VAL);
+    T = rot90(T');
+
+    YR = real(T);
+    YI = imag(T);
+    Y_mg = sqrt(YR.^2 + YI.^2)/2;
+
+    min = inf;
+    index = -1;
+     for j = 1:40
+        euclideanDistance = norm(real(Y_mg) - real(FeatureVectorMap(j).vec));
+        if (min > euclideanDistance)
+            min = euclideanDistance;
+            index = j;
+        end
+    end
+
+    disp(['Image test: ', int2str(i),'  Image set: ', int2str(index)])
+end
 
 
