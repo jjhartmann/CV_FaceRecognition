d3ed98cad840f1edd49a5c57c7c02e9a79fecb3f
jhartman <jhartman@sfu.ca>
2016-04-07 22:38:32 -0700

    Fix: issue with undeclared var.

--- extractFeatures.m
+++ extractFeatures.m
@@ -1,10 +1,10 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Function to extract image features using FFT
-function [Y] = extractFeatures(image)
+function [Y] = extractFeatures(image, K_VAL)
 
 % Pad image to a power of 2
 [n m] = size(image);
-imgpad = padarray(img, [0, floor(128-m)/2], 'replicate', 'both');
+imgpad = padarray(image, [0, floor(128-m)/2], 'replicate', 'both');
 imgpad = padarray(imgpad', [0 floor(128-n)/2], 'replicate', 'both')';
 
 % Get frequency Information
@@ -16,7 +16,7 @@ YI = imag(Y);
 Y = sqrt(YR.^2 + YI.^2)/2;
 
 T = Y;
-Y = T((n/2):n, (m/2):m, 1);
+Y = T((n/2 - 2):n, (m/2 + 2):m, 1);
 
 % remove higher frequencies. 
 T = rot90(Y);
